cfd03410666b26f626e6c1624dce9066fbf848b3
fajarfh <roseorchiddawn@gmail.com>
2020-02-27 12:34:32 +0700

    Assignment Chapter 2
    Push exact chapter 1 project to remote repository

---
+++ Assets/BallControl.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+	
+	// Rigidbody 2D bola
+	private Rigidbody2D rigidBody2D;
+
+	// Besarnya gaya awal yang diberikan untuk mendorong bola
+	public float xInitialForce;
+	public float yInitialForce;
+	public float initSpeed;
+
+	// RESET BOLA
+	void ResetBall()
+	{
+		// Reset posisi menjadi (0,0)
+		transform.position = Vector2.zero;
+
+		// Reset kecepatan menjadi (0,0)
+		rigidBody2D.velocity = Vector2.zero;
+	}
+
+	// DORONG BOLA
+	void PushBall()
+	{
+		// Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+		float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+		// Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+		float randomDirection = Random.Range(0, 2);
+
+		// Jika nilainya di bawah 1, bola bergerak ke kiri. 
+		// Jika tidak, bola bergerak ke kanan.
+		if (randomDirection < 1.0f)
+		{
+			// Gunakan gaya untuk menggerakkan bola ini.
+			Vector2 initForce = new Vector2(-xInitialForce, yRandomInitialForce).normalized;
+			rigidBody2D.AddForce(initForce * initSpeed);
+		}
+		else
+		{
+			Vector2 initForce = new Vector2(xInitialForce, yRandomInitialForce).normalized;
+			rigidBody2D.AddForce(initForce * initSpeed);
+		
+		}
+	}
+
+	// ACTIVATE BALLS
+	void activ8()
+	{
+		gameObject.SetActive(true);
+		PushBall();
+	}
+
+	// RESTART
+	void RestartGame()
+	{
+		// Kembalikan bola ke posisi semula
+		ResetBall();
+
+		if (IsInvoking())
+		{
+			CancelInvoke();
+		}
+
+		// Setelah 2 detik, berikan gaya ke bola
+		if (gameObject.name == "Ball")
+		{
+			Invoke("PushBall", 2);
+		}
+		else if (gameObject.name == "Bonus Ball")
+		{
+			gameObject.SetActive(false);
+			Invoke("activ8", 10);
+		}
+		else if (gameObject.name == "Bomb Ball")
+		{
+			gameObject.SetActive(false);
+			Invoke("activ8", 14);
+		}
+
+
+		}
+
+
+	// DEBUG
+	// Titik asal lintasan bola saat ini
+	private Vector2 trajectoryOrigin;
+
+	// Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		trajectoryOrigin = transform.position;
+	}
+
+	// Untuk mengakses informasi titik asal lintasan
+	public Vector2 TrajectoryOrigin
+	{
+		get { return trajectoryOrigin; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D>();
+
+		trajectoryOrigin = transform.position;
+
+		// Mulai game
+		RestartGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	// Tombol untuk menggerakkan ke atas
+	public KeyCode upButton = KeyCode.W;
+
+	// Tombol untuk menggerakkan ke bawah
+	public KeyCode downButton = KeyCode.S;
+
+	// Kecepatan gerak
+	public float speed = 10.0f;
+
+	// Batas atas dan bawah game scene (Batas bawah menggunakan minus (-))
+	public float yBoundary = 9.0f;
+
+	// Rigidbody 2D raket ini
+	private Rigidbody2D rigidBody2D;
+
+	// RESET RAKET
+	private Vector3 posisiAwal;
+
+	public void ResetRaket()
+	{
+		// Reset posisi
+		transform.position = posisiAwal;
+
+	}
+
+
+	// SCORE FUNCTION
+	// Skor pemain
+	private int score;
+
+	// Menaikkan skor sebanyak 1 poin
+	public void IncrementScore()
+	{
+		score++;
+	}
+
+	// Menurunkan skor sebanyak 1 poin
+	public void DecreaseScore()
+	{
+		score--;
+	}
+
+	private void ScaleUp()
+	{
+		transform.localScale += new Vector3(0, 4);
+		yBoundary -= 4.0f;
+	}
+
+	private void ScaleDown()
+	{
+		transform.localScale -= new Vector3(0, 4);
+		yBoundary += 4.0f;
+	}
+
+	public void PowerUp()
+	{
+		ScaleUp();
+		Invoke("ScaleDown", 10);
+	}
+
+	// Mengembalikan skor menjadi 0
+	public void ResetScore()
+	{
+		score = 0;
+	}
+
+	// Mendapatkan nilai skor
+	public int Score
+	{
+		get { return score; }
+	}
+
+	// CONTACT POINT DEBUG
+	// Titik tumbukan terakhir dengan bola, untuk menampilkan variabel-variabel fisika terkait tumbukan tersebut
+	private ContactPoint2D lastContactPoint;
+
+	// Untuk mengakses informasi titik kontak dari kelas lain
+	public ContactPoint2D LastContactPoint
+	{
+		get { return lastContactPoint; }
+	}
+
+	// Ketika terjadi tumbukan dengan bola, rekam titik kontaknya.
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.name.Equals("Ball"))
+		{
+
+			lastContactPoint = collision.GetContact(0);
+
+		} else if(collision.gameObject.name.Equals("Bonus Ball"))
+		{
+			
+			PowerUp();
+			collision.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+
+		} else if (collision.gameObject.name.Equals("Bomb Ball"))
+		{
+
+			DecreaseScore();
+			collision.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D>();
+		posisiAwal = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// CONTROL
+		// Dapatkan kecepatan raket sekarang.
+		Vector2 velocity = rigidBody2D.velocity;
+
+		// Jika pemain menekan tombol ke atas, beri kecepatan positif ke komponen y (ke atas).
+		if (Input.GetKey(upButton))
+		{
+			velocity.y = speed;
+		}
+
+		// Jika pemain menekan tombol ke bawah, beri kecepatan negatif ke komponen y (ke bawah).
+		else if (Input.GetKey(downButton))
+		{
+			velocity.y = -speed;
+		}
+
+		// Jika pemain tidak menekan tombol apa-apa, kecepatannya nol.
+		else
+		{
+			velocity.y = 0.0f;
+		}
+
+		// Masukkan kembali kecepatannya ke rigidBody2D.
+		rigidBody2D.velocity = velocity;
+
+		// BOUNDARY
+		// Dapatkan posisi raket sekarang.
+		Vector3 position = transform.position;
+
+		// Jika posisi raket melewati batas atas (yBoundary), kembalikan ke batas atas tersebut.
+		if (position.y > yBoundary)
+		{
+			position.y = yBoundary;
+		}
+
+		// Jika posisi raket melewati batas bawah (-yBoundary), kembalikan ke batas atas tersebut.
+		else if (position.y < -yBoundary)
+		{
+			position.y = -yBoundary;
+		}
+
+		// Masukkan kembali posisinya ke transform.
+		transform.position = position;
+
+
+	}
+
+}
+
+
